7e02594dd14920052563a8c27c6c0de585557691
subharthi75 <subharthi75@gmail.com>
2024-11-17 22:07:40 +0530

    hggh

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 // app.use(cors(corsOptions));
 
 // Middleware for CORS with conditional origin handling
-app.use(cors({ origin: true }));
+app.use(cors({ origin: "*" }));
 
 app.use(cookieParser());
 // app.use(cookieMiddleware);
