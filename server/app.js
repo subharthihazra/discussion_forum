d5f1e04dd62fb3f57a378237585ce8f7d166f6a2
subharthi75 <subharthi75@gmail.com>
2024-11-17 22:14:03 +0530

    srgte

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-import dotenv from "dotenv"; // Import dotenv
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import connectDB from "./db/db.js"; // Note the .js extension
-import postRoutes from "./routes/postRoutes.js";
-import cookieMiddleware from "./middleware/cookieMiddleware.js";
+import dotenv from 'dotenv'; // Import dotenv
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import connectDB from './db/db.js';  // Note the .js extension
+import postRoutes from './routes/postRoutes.js';
+import cookieMiddleware from './middleware/cookieMiddleware.js';
 
 dotenv.config();
 
@@ -12,36 +12,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-// const corsOptions = {
-//   origin: process.env.FRONTEND_URL, // Update this to your frontend's URL
-//   credentials: true, // Allow credentials
-// };
-// app.use(cors(corsOptions));
-
-// Middleware for CORS with conditional origin handling
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-    exposedHeaders: ["set-cookie"],
-  })
-);
-
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // Update this to your frontend's URL
+  credentials: true, // Allow credentials
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 // app.use(cookieMiddleware);
 app.use(express.json());
 
 // Routes
-app.use("/api/posts", postRoutes);
+app.use('/api/posts', postRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 // Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something broke!");
+  res.status(500).send('Something broke!');
 });
 
 // Connect to MongoDB and start the server
@@ -52,6 +42,6 @@ connectDB()
     });
   })
   .catch((error) => {
-    console.error("Failed to connect to MongoDB:", error);
+    console.error('Failed to connect to MongoDB:', error);
     process.exit(1); // Exit the process if the connection fails
   });
